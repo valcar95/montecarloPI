5cf2687992fe13a83d861caf30cf2a48aa748e2a
Victor Cardona <va.cv95@gmail.com>
2020-10-06 16:50:15 -0500

    paralelizacion del método

---
+++ functions.c
@@ -0,0 +1,35 @@
+/*****************************************************************************/
+/* Estimación del número Pi usando el método de Montecarlo                   */
+/* Versión secuencial                                                        */
+/* Autor: Danny Múnera                                                       */
+/* Curso de programación paralela con OpenMP                                 */
+/* 21/07/2020                                                                */
+/*****************************************************************************/
+#include "functions.h"
+
+double montecarlo_seq (int p) {  
+  int i, counter = 0;
+  double x, y, z, pi;
+  for(i = 0; i < p; ++i) {
+    x = (double)rand() / RAND_MAX;
+    y = (double)rand() / RAND_MAX;
+    z = x * x + y * y;
+    if( z <= 1 ) counter++;
+  }
+  pi = (double) counter / p * 4;
+  return pi;
+}
+
+double montecarlo_par (int p, int n_threads, unsigned int *seed) {  
+  int i, counter = 0;
+  double x, y, z, pi;
+  #pragma omp parallel for num_threads(n_threads) reduction(+:counter) private(i,x,y,z)
+  for(i = 0; i < p; ++i) {
+    x = (double)rand_r(seed) / RAND_MAX;
+    y = (double)rand_r(seed) / RAND_MAX;
+    z = x * x + y * y;
+    if( z <= 1 ) counter++; 
+  }
+  pi = (double) counter / p * 4;
+  return pi;
+}
